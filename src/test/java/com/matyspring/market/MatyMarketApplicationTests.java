5557807f5bb1f240415be1bad81c3f302f9deb74
Preiti Matias <preitimatias@gmail.com>
2020-12-06 16:56:52 -0300

    integracion con DB

---
+++ src/test/java/com/matyspring/market/MatyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matyspring.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
