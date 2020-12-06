953137238700178216554fb2ce7bcbbe733a3fc5
Preiti Matias <preitimatias@gmail.com>
2020-12-06 19:57:24 -0300

    prueba CRUD

---
+++ src/main/java/com/matyspring/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,19 @@
+package com.matyspring.market.persistence.crud;
+
+import com.matyspring.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+
+  //Forma Nativa  @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    //Query method //
+     List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+
+     Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
