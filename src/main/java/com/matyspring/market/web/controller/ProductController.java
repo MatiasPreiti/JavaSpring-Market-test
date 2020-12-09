0ae78a123b4334160199006061a7a060eb2aec2a
Preiti Matias <preitimatias@gmail.com>
2020-12-08 21:25:45 -0300

    Arreglo de CategoryMapper. refactor. implementacion de SpringSecurity y Swwager

--- src/main/java/com/matyspring/market/web/controller/ProductController.java
+++ src/main/java/com/matyspring/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.matyspring.market.web.controller;
 
 import com.matyspring.market.domain.Product;
 import com.matyspring.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +22,17 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @ApiOperation("Get all market product")
+    @ApiResponse(code = 200, message = "ok")
     public ResponseEntity<List<Product>> getAll(){
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Search a market product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "ok"),
+            @ApiResponse(code = 404, message = "producto no encontrado")})
     public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
         return productService.getProduct(productId).map(
                 product -> new ResponseEntity<>(product, HttpStatus.OK))
@@ -30,7 +40,8 @@ public class ProductController {
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+    public ResponseEntity<List<Product>> getByCategory(@ApiParam(value = "Id del producto", required = true, example = "7")
+                                                           @PathVariable("categoryId") int categoryId){
         return productService.getByCategory(categoryId)
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
